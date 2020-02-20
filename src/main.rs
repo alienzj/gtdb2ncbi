70df41c4253e2ebd153234389e0d321c99dfa99f
alienzj <alienchuj@gmail.com>
2020-02-21 01:31:20 +0800

    refactor

--- src/main.rs
+++ src/main.rs
@@ -8,30 +8,36 @@ fn main() {
     const ARCHAEA_XLSX: &[u8] = include_bytes!("../data/ncbi_vs_gtdb_archaea.xlsx");
     const BACTERIA_XLSX: &[u8] = include_bytes!("../data/ncbi_vs_gtdb_bacteria.xlsx");
 
-    let archaea_reader = Cursor::new(ARCHAEA_XLSX);
-    let bacteria_reader = Cursor::new(BACTERIA_XLSX);
+    let archaea_map = parse(ARCHAEA_XLSX);
+    let bacteria_map = parse(BACTERIA_XLSX);
 
-    let mut archaea_excel: Xlsx<_> = Xlsx::new(archaea_reader).unwrap();
-    let mut bacteria_excel: Xlsx<_> = Xlsx::new(bacteria_reader).unwrap();
+    println!("{:?}", archaea_map);
+    println!("{:?}", bacteria_map);
+}
 
-    let mut archaea_map: HashMap<String, Vec<String>> = HashMap::new();
+fn parse(raw_xlsx: &[u8]) -> HashMap<String, Vec<String>> {
+    let reader = Cursor::new(raw_xlsx);
 
-    for s in archaea_excel.sheet_names().to_owned() {
-        if let Some(Ok(range)) = archaea_excel.worksheet_range(&s) {
+    let mut excel: Xlsx<_> = Xlsx::new(reader).unwrap();
+
+    let mut map: HashMap<String, Vec<String>> = HashMap::new();
+
+    for s in excel.sheet_names().to_owned() {
+        if let Some(Ok(range)) = excel.worksheet_range(&s) {
             for r in range.rows().skip(1) {
-                if let Some(tax) = r[3].get_string() {
-                    for lineage in tax.split(',') {
+                if let Some(gtdb) = r[3].get_string() {
+                    for lineage in gtdb.split(',') {
                         if lineage.contains('(') {
                             let lineages: Vec<&str> = lineage.trim().split(|c| c == '(' || c == ')').collect();
                             if let Some(ncbi) = r[0].get_string() {
-                                println!("{}\t{}\t{}\n", ncbi, lineages[0], lineages[1]);
-                                archaea_map.insert(String::from(lineages[0]), vec!(String::from(ncbi), String::from(lineages[1])));
+                                // println!("{}\t{}\t{}\n", ncbi, lineages[0], lineages[1]);
+                                map.insert(String::from(lineages[0]), vec!(String::from(ncbi), String::from(lineages[1])));
                             }
                         } else {
                             let lineages: Vec<&str> = lineage.trim().rsplitn(2, ' ').collect();
                             if let Some(ncbi) = r[0].get_string() {
-                                println!("{}\t{}\n", ncbi, lineages[1]);
-                                archaea_map.insert(String::from(lineages[1]), vec!(String::from(ncbi)));
+                                // println!("{}\t{}\n", ncbi, lineages[1]);
+                                map.insert(String::from(lineages[1]), vec!(String::from(ncbi)));
                             }
                         }
                     }
@@ -39,8 +45,5 @@ fn main() {
             }
         }
     }
-
-    assert_eq!(archaea_excel.sheet_names(), bacteria_excel.sheet_names());
-    println!("{:?}", archaea_map);
-
+    map
 }
